c83bede6adfc7166415eec0c5eac1a7e72c95de2
Yue <wiloon.wy@gmail.com>
2023-05-05 22:06:40 +0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 package_name="mall-api"
+echo "building"
 GOOS=linux GOARCH=amd64 go build -o ${package_name} main.go
-
+echo "stop service"
 ansible -i 'wiloon.com,' all  -m shell -a 'systemctl stop mall' -u root
 scp config-aliyun.yaml aliyun:/data/mall/config.yaml
 scp ${package_name} aliyun:/data/mall
+echo "start service"
 ansible -i 'wiloon.com,' all  -m shell -a 'systemctl start mall' -u root
 
